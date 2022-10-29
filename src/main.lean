f007984544297c3f45ed60eff6803d417934e883
JasonKYi <kexing.ying19@imperial.ac.uk>
2022-10-29 16:15:38 +0100

    remove seed

--- src/main.lean
+++ src/main.lean
@@ -4,15 +4,15 @@ open io
 
 /-! # Uniform generation -/
 
-def random_fin (n : ℕ) (seed : ℕ) : io (fin n.succ) := 
-io.run_rand_with seed (rand.random $ fin n.succ)
+def random_fin (n : ℕ) : io (fin n.succ) := 
+io.run_rand (rand.random $ fin n.succ)
 
 -- Given some seed, we generate a random number in `fin n.succ = {0, ..., n}`.
-#eval random_fin 37 0 >>= print
+#eval random_fin 37 >>= print
 
 -- Generate a list of random numbers in `fin n.succ` of size `m`.
-def random_fin_list (n : ℕ) (m : ℕ) (seed : ℕ) : list (io $ fin n.succ) :=
-list.map (random_fin n) (list.range' seed (m + seed)) 
+def random_fin_list (n : ℕ) (m : ℕ) : list (io $ fin n.succ) :=
+list.map (λ _, random_fin n) (list.range m) 
 
 def print_list_io {α : Type*} [has_to_string α] : list (io α) → io (list α)
 | [] := return []
@@ -21,7 +21,7 @@ def print_list_io {α : Type*} [has_to_string α] : list (io α) → io (list α
   xs' ← print_list_io xs,
   return (x' :: xs')
 
-#eval print_list_io (random_fin_list 37 3 0) >>= print
+#eval print_list_io (random_fin_list 37 100) >>= print
 
 /-! # Generation given some distribution -/
 
